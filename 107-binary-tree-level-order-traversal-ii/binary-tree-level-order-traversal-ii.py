7ac157dbf4935b03d5f10f0257d9b6e0a51c9333
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2024-09-10 17:33:05 +0800

    Time: 3 ms (100.00%) | Memory: 12.1 MB (36.06%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        next_queue = []
+        level = []
+        result = []
+
+        while queue != []:
+            for root in queue:
+                level.append(root.val)
+                if root.left is not None:
+                    next_queue.append(root.left)
+                if root.right is not None:
+                    next_queue.append(root.right)
+            result.append(level)
+            level = []
+            queue = next_queue
+            next_queue = []
+        return result[::-1]
