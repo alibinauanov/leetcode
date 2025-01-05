0d598c5749eae69af0a3078d30275cdafa202b65
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2025-01-06 02:26:04 +0500

    Time: 6 ms (77.19%) | Memory: 12.8 MB (8.95%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -5,5 +5,18 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[List[int]]
         """
-        set_one, set_two = set(nums1), set(nums2)
-        return [list(set_one - set_two), list(set_two - set_one)]
+        nums1Set = set(nums1)
+        nums2Set = set(nums2)
+
+        subarr1 = set()
+        subarr2 = set()
+
+        for i in nums1:
+            if i not in nums2Set:
+                subarr1.add(i)
+        
+        for i in nums2:
+            if i not in nums1Set:
+                subarr2.add(i)
+        
+        return [list(subarr1), list(subarr2)]
