ff08da0e6eaeef654485d504a47321455dbd3dfb
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2025-08-01 16:26:15 +0800

    Time: 10 ms (95.29%) | Memory: 12.5 MB (73.92%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        res = []
+        board = [["."] * n for i in range(n)]
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        def dfs(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                dfs(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+            
+        dfs(0)
+        return res
