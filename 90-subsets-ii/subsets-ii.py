9149796e5f01c25ebd84ebbd668f7e4d36cab75a
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2025-10-04 23:20:01 +0500

    Time: 2 ms (39.42%) | Memory: 12.8 MB (34.59%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        res = []
-
-        def backtrack(start, path):
-            res.append(list(path))
-            for i in range(start, len(nums)):
-                if i > start and nums[i] == nums[i - 1]:
+        def backtrack(i):
+            res.append(sol[:])
+            for j in range(i, n):
+                if j > i and nums[j] == nums[j - 1]:
                     continue
-                path.append(nums[i])
-                backtrack(i + 1, path)
-                path.pop()
+                sol.append(nums[j])
+                backtrack(j + 1)
+                sol.pop()
         
         nums.sort()
-        backtrack(0, [])
+        n = len(nums)
+        res, sol = [], []
+        backtrack(0)
         return res
