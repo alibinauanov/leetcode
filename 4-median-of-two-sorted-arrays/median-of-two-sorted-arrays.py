4fb44add52b7f31e6de7d131b01df5b9868b8a2a
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2025-10-04 13:38:11 +0500

    Time: 12 ms (12.33%) | Memory: 12.7 MB (51.15%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -5,25 +5,25 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        nums = []
-        pointer1 = 0
-        pointer2 = 0
+        merge = []
+        n1 = 0
+        n2 = 0
 
-        while len(nums1) > pointer1 and len(nums2) > pointer2:
-            if nums1[pointer1] <= nums2[pointer2]:
-                nums.append(nums1[pointer1])
-                pointer1 += 1
+        while len(nums1) > n1 and len(nums2) > n2:
+            if nums1[n1] <= nums2[n2]:
+                merge.append(nums1[n1])
+                n1 += 1
             else:
-                nums.append(nums2[pointer2])
-                pointer2 += 1
+                merge.append(nums2[n2])
+                n2 += 1
         
-        if pointer1 < len(nums1):
-            nums.extend(nums1[pointer1:])
-        if pointer2 < len(nums2):
-            nums.extend(nums2[pointer2:])
-
-        mid = len(nums) // 2
-        if len(nums) % 2 == 0:
-            return (nums[mid - 1] + nums[mid]) / 2.0
+        if len(nums1) > n1:
+            merge.extend(nums1[n1:])
+        if len(nums2) > n2:
+            merge.extend(nums2[n2:])
+        
+        n = len(merge) // 2
+        if len(merge) % 2 == 0:
+            return (merge[n] + merge[n - 1]) / 2.0
         else:
-            return float(nums[mid])
+            return float(merge[n])
