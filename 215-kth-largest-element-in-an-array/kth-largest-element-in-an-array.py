816c7e1d5f6726a3ec6a00b65e3140091a1e6c92
Alibi Nauanov <106968134+alibinauanov@users.noreply.github.com>
2025-10-08 23:30:47 +0500

    Time: 681 ms (10.03%) | Memory: 19.5 MB (71.14%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -5,22 +5,9 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        # SORT APPROACH
-        nums.sort(reverse=True)
-        return nums[k-1]
-
-        # k = len(nums) - k
-
-        # def quickSelect(l, r):
-        #     pivot, p = nums[r], l
-        #     for i in range(l, r):
-        #         if nums[i] <= pivot:
-        #             nums[p], nums[i] = nums[i], nums[p]
-        #             p += 1
-        #     nums[p], nums[r] = nums[r], nums[p]
-
-        #     if p > k:   return quickSelect(l, p - 1) 
-        #     elif p < k: return quickSelect(p + 1, r)
-        #     else:       return nums[p]
-        
-        # return quickSelect(0, len(nums) - 1)
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
